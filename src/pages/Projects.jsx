edf8f363bd38039c6d25d64e7ff2ac2d0eacb416
Saransh Mehta <119345742+msaransh@users.noreply.github.com>
2023-07-30 16:38:52 +0530

    Update Projects.jsx

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -12,37 +12,6 @@ const Projects = () => {
             Projects
           </h1>
 
-          <div>
-            <h2 className="pt-10 text-xl md:text-3xl font-bold">
-              Student Teacher Booking Appointment System
-            </h2>
-
-            <div className="flex flex-col justify-between items-baseline">
-              <h3 className="pt-2 pb-5 font-semibold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-blue-400">
-                03/2023 - 03/2023
-              </h3>
-
-              {/* <li className="text-sm md:text-base text-justify">
-                Developed a Reminder App using React & Firebase in which you can
-                sign in with your email and password and set reminders for your
-                daily tasks.
-              </li> */}
-              
-              <br />
-              <div className="flex flex-row gap-5 ">
-                <a
-                  className="duration-300 hover:scale-105 group text-white w-fit px-3 py-2 flex items-center rounded-md bg-gradient-to-r from-purple-500 to-blue-400 cursor-pointer shadow-gray-700 shadow-lg"
-                  href={
-                    "https://github.com/msaransh/Student-Teacher-Booking-Appointment--System"
-                  }
-                  target="_blank"
-                >
-                  GitHub
-                </a>
-              </div>
-            </div>
-          </div>
-
           {/*Portfolio Website*/}
 
           <div>
@@ -62,13 +31,71 @@ const Projects = () => {
               <br />
               <a
                 className="duration-300 hover:scale-105 group text-white w-fit px-3 py-2 flex items-center rounded-md bg-gradient-to-r from-purple-500 to-blue-400 cursor-pointer shadow-gray-700 shadow-lg"
-                href={
-                  " "
-                }
+                href={" "}
                 target="_blank"
               >
                 GitHub
               </a>
+            </div>
+          </div>
+
+          <div>
+            <h2 className="pt-10 text-xl md:text-3xl font-bold">
+              Decentralised Password Manager
+            </h2>
+
+            <div className="flex flex-col justify-between items-baseline">
+              <h3 className="pt-2 pb-5 font-semibold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-blue-400">
+                02/2023 - 05/2023
+              </h3>
+              <li className="text-sm md:text-base text-justify">
+                Used Python and Mathematics to create an Algorithm which can
+                encrypt and divide a password into number of shares, and when
+                given back majority of those shares, can decrypt and retrieve
+                the same password.
+              </li>
+              <li className="text-sm md:text-base text-justify">
+                Learned how Decentralixsation works. 
+              </li>
+              <li className="text-sm md:text-base text-justify">
+                Learned to use Mathematics to make real life Algorithm.
+              </li>
+              <br />
+              {/* <div className="flex flex-row gap-5 ">
+                <a
+                  className="duration-300 hover:scale-105 group text-white w-fit px-3 py-2 flex items-center rounded-md bg-gradient-to-r from-purple-500 to-blue-400 cursor-pointer shadow-gray-700 shadow-lg"
+                  href={
+                    "https://github.com/msaransh/Student-Teacher-Booking-Appointment--System"
+                  }
+                  target="_blank"
+                >
+                  GitHub
+                </a>
+              </div> */}
+            </div>
+          </div>
+          <div>
+            <h2 className="pt-10 text-xl md:text-3xl font-bold">
+              Student Teacher Booking Appointment System
+            </h2>
+
+            <div className="flex flex-col justify-between items-baseline">
+              <h3 className="pt-2 pb-5 font-semibold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-blue-400">
+                03/2023 - 03/2023
+              </h3>
+
+              <br />
+              <div className="flex flex-row gap-5 ">
+                <a
+                  className="duration-300 hover:scale-105 group text-white w-fit px-3 py-2 flex items-center rounded-md bg-gradient-to-r from-purple-500 to-blue-400 cursor-pointer shadow-gray-700 shadow-lg"
+                  href={
+                    "https://github.com/msaransh/Student-Teacher-Booking-Appointment--System"
+                  }
+                  target="_blank"
+                >
+                  GitHub
+                </a>
+              </div>
             </div>
           </div>
         </div>
