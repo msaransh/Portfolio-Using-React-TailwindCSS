ba90a59d3f2cdb7dfc174dcd5d7956ef0fef20c6
Saransh Mehta <119345742+msaransh@users.noreply.github.com>
2024-02-21 03:15:17 +0530

    Update Projects.jsx

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -16,6 +16,60 @@ const Projects = () => {
 
           <div>
             <h2 className="pt-10 text-xl md:text-3xl font-bold">
+              Myntra Clone
+            </h2>
+
+            <div className="flex flex-col justify-between items-baseline">
+              <h3 className="pt-2 pb-5 font-semibold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-blue-400">
+                01/2024 - 02/2024
+              </h3>
+
+              <li className="text-sm md:text-base text-justify">
+                A responsive sample website of E-Commerce, built with React JS
+                and Redux, ensuring responsiveness and seamless state management
+                for optimal user engagement
+              </li>
+              <br />
+              <a
+                className="duration-300 hover:scale-105 group text-white w-fit px-3 py-2 flex items-center rounded-md bg-gradient-to-r from-purple-500 to-blue-400 cursor-pointer shadow-gray-700 shadow-lg"
+                href={
+                  " https://github.com/msaransh/msaransh-E-Commerce-Myntra-Clone "
+                }
+                target="_blank"
+              >
+                GitHub
+              </a>
+            </div>
+          </div>
+
+          <div>
+            <h2 className="pt-10 text-xl md:text-3xl font-bold">Todo List</h2>
+
+            <div className="flex flex-col justify-between items-baseline">
+              <h3 className="pt-2 pb-5 font-semibold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-blue-400">
+                01/2024
+              </h3>
+
+              <li className="text-sm md:text-base text-justify">
+                Web App made for creating to-do list for the day
+              </li>
+              <li className="text-sm md:text-base text-justify">
+                Features - Add, Delete, and Sort the todos lexographically and
+                in the order they were added.
+              </li>
+              <br />
+              <a
+                className="duration-300 hover:scale-105 group text-white w-fit px-3 py-2 flex items-center rounded-md bg-gradient-to-r from-purple-500 to-blue-400 cursor-pointer shadow-gray-700 shadow-lg"
+                href={" https://github.com/msaransh/To-do-App-using-React-JS "}
+                target="_blank"
+              >
+                GitHub
+              </a>
+            </div>
+          </div>
+
+          <div>
+            <h2 className="pt-10 text-xl md:text-3xl font-bold">
               Portfolio Website using ReactJs and Tailwind CSS
             </h2>
 
@@ -25,13 +79,16 @@ const Projects = () => {
               </h3>
 
               <li className="text-sm md:text-base text-justify">
-                Developed this personal portfolio website using technologies
-                like ReactJs, Tailwind CSS etc.
+                Designed and developd a personal portfolio website showcasing
+                projects and contact information using HTML, CSS, ReactJS, and
+                TailwindCSS.
               </li>
               <br />
               <a
                 className="duration-300 hover:scale-105 group text-white w-fit px-3 py-2 flex items-center rounded-md bg-gradient-to-r from-purple-500 to-blue-400 cursor-pointer shadow-gray-700 shadow-lg"
-                href={" "}
+                href={
+                  " https://github.com/msaransh/Portfolio-Using-React-TailwindCSS "
+                }
                 target="_blank"
               >
                 GitHub
@@ -55,7 +112,7 @@ const Projects = () => {
                 the same password.
               </li>
               <li className="text-sm md:text-base text-justify">
-                Learned how Decentralixsation works. 
+                Learned how Decentralixsation works.
               </li>
               <li className="text-sm md:text-base text-justify">
                 Learned to use Mathematics to make real life Algorithm.
@@ -83,7 +140,10 @@ const Projects = () => {
               <h3 className="pt-2 pb-5 font-semibold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-blue-400">
                 03/2023 - 03/2023
               </h3>
-
+              <li className="text-sm md:text-base text-justify">
+                It is a web-based application that allows students to book
+                appointments with their teachers online.
+              </li>
               <br />
               <div className="flex flex-row gap-5 ">
                 <a
@@ -105,3 +165,4 @@ const Projects = () => {
 };
 
 export default Projects;
+
